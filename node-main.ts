fc5334c21f1f0db49933d9b4aea4ed9da3493506
techieatul <atul.nitt1994@gmail.com>
2022-04-14 00:00:36 -0700

    Error corrected

--- node-main.ts
+++ node-main.ts
@@ -5,18 +5,21 @@ var importObject = {
   imports: {
     print_num: (arg : any) => {
       console.log("Logging from WASM: ", arg);
-      //display(String(arg));
+      console.log(String(arg));
       return arg;
     },
     print_bool: (arg : any) => {
       if(arg === 0) { //display("False"); 
+        console.log(String(arg));
       }
       else { //display("True"); 
+        console.log(String(arg));
       }
       return arg;
     },
     print_none: (arg: any) => {
      // display("None");
+     console.log("None")
       return arg;
     }
   },
@@ -27,9 +30,10 @@ var importObject = {
 var source:string = "def f(x:int)->int:\n\tif x>=4:\n\t\treturn 1\n\tx=x-1\n\tf(x)\nf(2)"
 
 
+
 // node node-main.js 987
 const input = process.argv[2];
-const result = compile(input);
+const result = compile(source);
 console.log(result);
 run(result, {}).then((value) => {
   console.log(value);
